fd5e93ffbe90f887c6b0eda7e43cf9cca8b2cf6a
Elif Reisoglu <elifreisogluu95@gmail.com>
2022-04-26 22:44:30 +0300

    flutter full learn 3 appbar learning

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
           actionsIconTheme:  IconThemeData(color: Colors.pink),
         )
       ),
-      home: const ButtonLearn()
+      home: const AppBarLearnView()
       
     );
   }
