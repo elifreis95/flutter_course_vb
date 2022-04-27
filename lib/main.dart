65e5e87c7d50f25de9b6f1d47338291a46fbe86b
Elif Reisoglu <elifreisogluu95@gmail.com>
2022-04-27 18:49:08 +0300

    flutter full learn 4 card learning

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,7 @@ class MyApp extends StatelessWidget {
           actionsIconTheme:  IconThemeData(color: Colors.pink),
         )
       ),
-      home:const PaddingLearn()
+      home:const CardLearn()
       
     );
   }
