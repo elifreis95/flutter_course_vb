39b44ff7d4ba453eeca3d46deb4b7112aca58aeb
Ali Rıza Reisoğlu <alirizareisoglu@gmail.com>
2022-06-09 12:39:54 +0300

    new mac commits

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,9 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_full_learn/101/container_sized_box_learn.dart';
-import 'package:flutter_full_learn/101/scaffold_learn.dart';
-import 'package:flutter_full_learn/101/text_learn_view.dart';
 
-import '101/app_bar_learn.dart';
-import '101/button_learn.dart';
-import '101/card_learn.dart';
 import '101/color_learn.dart';
-import '101/column_row_learn.dart';
-import '101/custom_widget_learn.dart';
-import '101/icon_learn.dart';
-import '101/image_learn.dart';
-import '101/indicator_learn.dart';
-import '101/list_tile_learn.dart';
-import '101/list_view_builder.dart';
-import '101/list_view_learn.dart';
-import '101/navigation_learn.dart';
-import '101/padding_learn.dart';
-import '101/page_view_learn.dart';
-import '101/stack_learn.dart';
-import '101/stateful_learn.dart';
-import '101/stateful_life_cycle_learn.dart';
-import '101/stateless_learn.dart';
-import '101/text_field_learn.dart';
 import '202/model_learn_view.dart';
-import '202/tab_learn.dart';
-import 'demos/color_demos_view.dart';
-import 'demos/color_life_cycle_view.dart';
-import 'demos/my_collections_demos.dart';
-import 'demos/note_demos_view.dart';
-import 'demos/stack_demo_view.dart';
 
 
 void main() {
