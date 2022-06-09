df27f1c2a1a31f376b02be2e370b527c313ff09f
Ali Rıza Reisoğlu <alirizareisoglu@gmail.com>
2022-06-09 12:46:34 +0300

    user changed

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 import '101/color_learn.dart';
 import '202/model_learn_view.dart';
-
 
 void main() {
   runApp(const MyApp());
@@ -17,66 +15,41 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-    
-      title: 'Flutter Demo',
-      
-      debugShowCheckedModeBanner: false,
-      
-      theme: ThemeData.dark().copyWith(
-        tabBarTheme: TabBarTheme(
-          labelColor: Colors.black,
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.dark().copyWith(
+            tabBarTheme: TabBarTheme(
+              labelColor: Colors.black,
               unselectedLabelColor: Colors.white,
               indicatorSize: TabBarIndicatorSize.label,
-        ),
-        bottomAppBarTheme: BottomAppBarTheme(
-          shape: CircularNotchedRectangle()
-        ),
-        progressIndicatorTheme: ProgressIndicatorThemeData(
-          
-          color: Colors.white,
-          ),
-          listTileTheme: ListTileThemeData(
-            contentPadding: EdgeInsets.zero),
-        cardTheme: CardTheme(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(20))
-        ),
-        
-        errorColor:  ColorsItems.deluge,
-        textSelectionTheme: TextSelectionThemeData(
-          selectionColor: Colors.red, 
-          cursorColor: Colors.green,
-          selectionHandleColor: Colors.black
-
-        ),
-        inputDecorationTheme: InputDecorationTheme(
-          filled: true,
-          fillColor: Colors.white,
-          iconColor: Colors.red,
-          labelStyle: TextStyle(color: Colors.purple),
-          border: OutlineInputBorder(),
-          floatingLabelStyle: TextStyle(
-            color: Colors.red,
-            fontSize: 20,
-            fontWeight: FontWeight.w600
-          ),
-        
-        ),
-        textTheme: TextTheme(subtitle1: TextStyle(color: Colors.green),
-        ),
-        
-        appBarTheme: const  AppBarTheme(
-          centerTitle: true,
-          systemOverlayStyle: SystemUiOverlayStyle.light,
-          backgroundColor: Colors.transparent,
-          elevation: 0,
-          actionsIconTheme:  IconThemeData(
-            color: Colors.white),
-        )
-      ),
-      home: ModelLearnView()
-      
-    );
+            ),
+            bottomAppBarTheme: BottomAppBarTheme(shape: CircularNotchedRectangle()),
+            progressIndicatorTheme: ProgressIndicatorThemeData(
+              color: Colors.white,
+            ),
+            listTileTheme: ListTileThemeData(contentPadding: EdgeInsets.zero),
+            cardTheme: CardTheme(shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20))),
+            errorColor: ColorsItems.deluge,
+            textSelectionTheme: TextSelectionThemeData(
+                selectionColor: Colors.red, cursorColor: Colors.green, selectionHandleColor: Colors.black),
+            inputDecorationTheme: InputDecorationTheme(
+              filled: true,
+              fillColor: Colors.white,
+              iconColor: Colors.red,
+              labelStyle: TextStyle(color: Colors.purple),
+              border: OutlineInputBorder(),
+              floatingLabelStyle: TextStyle(color: Colors.red, fontSize: 20, fontWeight: FontWeight.w600),
+            ),
+            textTheme: TextTheme(
+              subtitle1: TextStyle(color: Colors.green),
+            ),
+            appBarTheme: const AppBarTheme(
+              centerTitle: true,
+              systemOverlayStyle: SystemUiOverlayStyle.light,
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+              actionsIconTheme: IconThemeData(color: Colors.white),
+            )),
+        home: ModelLearnView());
   }
 }
-
